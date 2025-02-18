3105a91df01d9b96e2373e653c26f4f3c1e46c2f
Asifraza0911 <asifraza061106@gmail.com>
2025-02-18 09:56:31 +0530

    Add environment templates and update gitignore

---
+++ src/environments/environment.template.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'YOUR_API_KEY',
+    authDomain: 'YOUR_AUTH_DOMAIN',
+    projectId: 'YOUR_PROJECT_ID',
+    storageBucket: 'YOUR_STORAGE_BUCKET',
+    messagingSenderId: 'YOUR_SENDER_ID',
+    appId: 'YOUR_APP_ID',
+    measurementId: 'YOUR_MEASUREMENT_ID'
+  },
+  huggingface: {
+    apiKey: 'YOUR_HUGGINGFACE_KEY'
+  }
+}; 
